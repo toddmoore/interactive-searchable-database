e5dcdc1f31fa0769655e60f1d91de46698e2c60b
Todd Moore <todd.moore@guardian.co.uk>
2015-03-22 13:02:35 +1100

    adding lastest

--- src/lib/components/dataTable.js
+++ src/lib/components/dataTable.js
@@ -1,9 +1,40 @@
-import testTemplate from '../../templates/components/dataTable.html.txt!text'
+import dataTableTemplate from
+	'../../templates/components/dataTable.html.txt!text'
 
-class TestComponent {
-  constructor() {
-    this.template = testTemplate;
-  }
+class DataTableComponent {
+	constructor() {
+		this.template = dataTableTemplate;
+		this.isolated = false;
+		this.data = function() {
+			return {
+				area: null,
+				title: null,
+				keys: [],
+				values: []
+			};
+		};
+	}
+
+	init() {
+		this.set("areaLength", this.get("area").length);
+		if (this.get("areaLength")) {
+			var parentValues = [];
+			this.get("area").forEach((item) => {
+				console.log(this.get("area"), this.get("values"))
+				var keys = [];
+				var values = [];
+				for (var key in item) {
+					if (key != "$$hashKey") {
+						keys.push(key);
+						values.push(item[key]);
+					}
+				}
+				parentValues.push(values)
+				this.set("keys", keys);
+			});
+			this.set("values", parentValues);
+		}
+	}
 }
 
-export default TestComponent;
+export default DataTableComponent;
--- src/lib/index.js
+++ src/lib/index.js
@@ -6,62 +6,88 @@ import TypeAhead from 'typeahead.js';
 import Datums from '../data.json.txt!text';
 import Sample from '../sample.json.txt!text';
 
+var key = "1d41uUjUpyzX5SVJGgo7RZfOTmOWBMzBzPhFOLHRUOSE";
 var datums = JSON.parse(Datums);
 var sample = JSON.parse(Sample);
 
-// Bloodhound
-var bloodhound = new Bloodhound({
-  name: 'animals',
-  local: datums,
-  datumTokenizer: function(d) {
-    return Bloodhound.tokenizers.whitespace(d.lastname);
-  },
-  queryTokenizer: Bloodhound.tokenizers.whitespace
-});
-bloodhound.initialize();
-
 class App {
-  constructor() {
-    this.el = document.querySelector('.interactive');
-    this.template = mainTemplate;
-    this.data = {
-      "subtitle": "whatever"
-    }
-    this.components = {
-      DataTable: Ractive.extend(new dataTable())
-    }
-  }
+	constructor(translations, bloodhound) {
+		this.bloodhound = bloodhound;
+		this.el = document.querySelector('.interactive');
+		this.template = mainTemplate;
+		this.data = {
+			"subtitle": "whatever",
+			"translate": this.handleTranslation,
+			"translation": translations
+		}
+		this.components = {
+			DataTable: Ractive.extend(new dataTable())
+		}
+	}
 
-  handleMPNameChange(newValue, oldValue, keypath) {
-    this.set("selectedPolitician", null);
-    bloodhound.get(newValue, (suggestions) => {
-      this.set("suggestions", suggestions);
-    });
-  }
+	handleTranslation(key) {
+		return this.get("translation")[key] || `<< NO TRANSLATION for ${key} >>`;
+	}
 
-  setMp(mp) {
-    var id = mp.context.id;
-    var keys = [];
-    for (var key in sample[id]) {
-      keys.push(key);
-    }
-    this.set("titles", keys);
-    this.set("selectedPolitician", true);
-    this.set("politician", mp.context);
-    this.set("politicianData", sample[id])
-    console.log(this.get("politicianData"));
-    this.set("mpname", id);
-  }
+	handleMPNameChange(newValue, oldValue, keypath) {
+		this.bloodhound.get(newValue, (suggestions) => {
+			this.set("suggestions", suggestions);
+		});
+	}
+
+	setMp(mp) {
+		this.set("selectedPolitician", false);
+		this.set("politician", false);
+		this.set("politicianData", []);
+		this.set("mpname", "");
+		var id = mp.context.id;
+		this.set("selectedPolitician", true);
+		this.set("politician", mp.context);
+		this.set("politicianData", sample[id]);
+		this.set("mpname", id);
+		window.location.hash = "#results";
+	}
 }
 
+var translations = new Promise((resolved, rejected) => {
+	jquery.getJSON(`http://interactive.guim.co.uk/spreadsheetdata/${key}.json`, (
+		data) => {
+		// TODO: add error
+		data = data.sheets.Sheet1;
+		var obj = {};
+		data.forEach((item) => {
+			var _key_ = null;
+			var _value_ = null;
+			for (var key in item) {
+				if (key == "key") {
+					_key_ = item[key];
+				} else {
+					_value_ = item[key];
+				}
+			}
+			obj[_key_] = _value_;
+		});
+		resolved(obj);
+	})
+}).then((data) => {
+	// Bloodhound
+	var bloodhound = new Bloodhound({
+		name: 'animals',
+		local: datums,
+		datumTokenizer: function(d) {
+			return Bloodhound.tokenizers.whitespace(d.lastname);
+		},
+		queryTokenizer: Bloodhound.tokenizers.whitespace
+	});
+	bloodhound.initialize();
 
-// Instantiate the app
-var app = new App();
-var ractive = new Ractive(app);
+	var app = new App(data, bloodhound);
+	var ractive = new Ractive(app);
 
-// attach proxy
-ractive.on('mpselect', app.setMp);
-// attach observers
-ractive.observe("mpname", app.handleMPNameChange);
+	// attach proxy
+	ractive.on('mpselect', app.setMp);
+	// attach observers
+	ractive.observe("mpname", app.handleMPNameChange);
+});
 
 export default {};
