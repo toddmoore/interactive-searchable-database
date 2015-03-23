83cdbf10a7c21505b063825218ad2002a1647b4c
Todd Moore <todd.moore@guardian.co.uk>
2015-03-23 16:35:10 +1100

    adding twitter and facebook

--- src/boot.js
+++ src/boot.js
@@ -14,26 +14,34 @@ define([], function() {
 		boot: function(el, context, config, mediator) {
 			// Load CSS
 			addCSS('http://pasteup.guim.co.uk/0.0.5/css/fonts.pasteup.min.css');
-			addCSS('css/reset.css');
+			addCSS(
+				'http://interactive.guim.co.uk/next-gen/au/2015/mar/interactive-searchable/css/reset.css'
+			);
 			addCSS(
 				'http://interactive.guim.co.uk/visuals-blank-page/guardian-poll-projections/assets-1426693564091/css/main.css'
 			);
 			addCSS(
 				'http://assets.guim.co.uk/stylesheets/0a849da71459dc6551f21cd90a47e38f/global.css'
 			);
-			addCSS('css/main.css');
+			addCSS(
+				'http://interactive.guim.co.uk/next-gen/au/2015/mar/interactive-searchable/css/main.css'
+			);
 
 
 			// Add Traceur Runtime
 			var traceur = document.createElement('script');
 			traceur.setAttribute("type", "text/javascript");
-			traceur.setAttribute("src", "traceur-runtime.js");
+			traceur.setAttribute("src",
+				"http://interactive.guim.co.uk/next-gen/au/2015/mar/interactive-searchable/traceur-runtime.js"
+			);
 			document.getElementsByTagName("head")[0].appendChild(traceur);
 
 			// Add minified build
 			var build = document.createElement('script');
 			build.setAttribute("type", "text/javascript");
-			build.setAttribute("src", "build.js");
+			build.setAttribute("src",
+				"http://interactive.guim.co.uk/next-gen/au/2015/mar/interactive-searchable/build.js"
+			);
 			document.getElementsByTagName("head")[0].appendChild(build);
 
 		}
